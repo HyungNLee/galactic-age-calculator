782706f395b56478bb8e0e4d2f8f28da9e90894e
Hyung Lee <hyungnaelee@gmail.com>
2018-10-12 10:05:05 -0700

    Add functionality to get the difference in days between input date-of-birth to current day. Add methods and tests for marsAge, venusAge, and mercuryAge.

---
+++ spec/age-converter-spec.js
@@ -0,0 +1,38 @@
+import { AgeConverter } from "../src/age-converter.js";
+
+describe('Age Converter', function() {
+
+  let inputDate;
+
+  beforeEach(function() {
+    let inputString = "1990-01-15";
+    let inputArray = inputString.split("-");
+    
+    inputDate = new AgeConverter(inputArray, 75);
+  });
+
+  it('should correctly construct AgeConverter object', function() {
+    let earthDays = inputDate.totalDaysAlive;
+    expect(earthDays).toEqual(10497);
+  });
+
+  it('should hold proper earth age value', function() {
+    let earthAge = inputDate.earthAge;
+    expect(earthAge).toEqual(28);
+  });
+
+  it('should test getMercuryAge() for proper value', function() {
+    let mercuryAge = inputDate.mercuryAge;
+    expect(mercuryAge).toEqual(116);
+  });
+
+  it('should test getVenusAge() for proper value', function() {
+    let venusAge = inputDate.venusAge;
+    expect(venusAge).toEqual(45);
+  });
+
+  it('should test getMarsAge() for proper value', function() {
+    let marsAge = inputDate.marsAge;
+    expect(marsAge).toEqual(14);
+  });
+});
---
+++ src/age-converter.js
@@ -0,0 +1,119 @@
+/* eslint-disable no-unused-vars */
+
+export class AgeConverter {
+  constructor(inputDate, lifeExpectancy) {
+    this.birthdayArray = inputDate;
+    this.year = parseInt(inputDate[0]);
+    this.month = parseInt(inputDate[1]) - 1;
+    this.day = parseInt(inputDate[2]);
+    this.totalDaysAlive = this.getDayDifference();
+    this.earthAge = this.getEarthAge();
+    this.mercuryAge = this.getMercuryAge();
+    this.venusAge = this.getVenusAge();
+    this.marsAge = this.getMarsAge();
+    this.jupiterAge;
+    this.lifeExpectancy = lifeExpectancy;
+  }
+
+  getDayDifference() {
+    let today = new Date();
+    let todayMonth = today.getMonth();
+    let todayDay = today.getDate();
+    let todayYear = today.getFullYear();
+
+    let totalYearDays = (this.year - todayYear) * 365;
+
+    let totalMonthDays = (this.getDaysinMonths(this.month, this.year) + this.day) - (this.getDaysinMonths(todayMonth, todayYear) + todayDay);
+
+    let totalDays = totalYearDays + totalMonthDays;
+    if (this.year < todayYear) {
+      totalDays -= this.countLeapYears(this.year, this.month, this.day, todayYear, todayMonth, todayDay);
+    } else {
+      totalDays += this.countLeapYears(this.year, this.month, this.day, todayYear, todayMonth, todayDay);
+    }
+    return Math.abs(totalDays);
+  }
+
+  checkLeapYear(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  countLeapYears(yearOne, monthOne, dayOne, yearTwo, monthTwo, dayTwo) {
+    let startYear = 0;
+    let startMonth = 0;
+    let startDay = 0;
+    let finalYear = 0;
+    let finalMonth = 0;
+    let finalDay = 0;
+    if (yearOne < yearTwo) {
+      startYear = yearOne;
+      startMonth = monthOne;
+      startDay = dayOne;
+      finalYear = yearTwo;
+      finalMonth = monthTwo;
+      finalDay = dayTwo;
+    } else if (yearTwo < yearOne) {
+      startYear = yearTwo;
+      startMonth = monthTwo;
+      startDay = dayTwo;
+      finalYear = yearOne;
+      finalMonth = monthOne;
+      finalDay = dayOne;
+    } else {
+      return 0;
+    }
+    
+    let count = 0;
+
+    for (let i = startYear; i <= finalYear; i++) {
+      if (this.checkLeapYear(i)) {
+        count++;
+      }
+    }
+
+    if (this.checkLeapYear(startYear) && (startMonth > 1)) {
+      count--;
+    }
+
+    if (this.checkLeapYear(finalYear) && ((finalMonth === 1 && finalDay <= 29) || finalMonth < 1)) {
+      count--;
+    }
+    return count;
+  }
+
+  getDaysinMonths(month) {
+    const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let totalMonthDays = 0;
+    for (let i = 0; i < month; i++)
+    {
+      totalMonthDays += monthDays[i]; 
+    }
+    return totalMonthDays;
+  }
+
+  getEarthAge() {
+    return Math.floor(this.totalDaysAlive/365);
+  }
+
+  getMercuryAge() {
+    return Math.floor(this.earthAge*(365/87.97));
+  }
+
+  getVenusAge() {
+    return Math.floor(this.earthAge*(365/224.70));
+  }
+
+  getMarsAge() {
+    return Math.floor(this.earthAge/1.8808476);
+  }
+
+  getJupiterAge() {
+
+  }
+
+  
+}
